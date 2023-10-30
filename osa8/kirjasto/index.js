7e1d4c0c4fe23f71b950f0c74b26923df5084827
Jenni Harakka <jvharakka@gmail.com>
2023-10-30 19:42:25 +0200

    8.11

--- osa8/kirjasto/index.js
+++ osa8/kirjasto/index.js
@@ -161,11 +161,13 @@ const resolvers = {
         return book
     },
     editAuthor: (root, args) => {
-        const author = authors.find( a => a.name === args.name)
+      console.log(args)
+        const author = authors.find(a => a.name === args.name)
         if(!author){
             return null
         }
         const updatedAuthor = {...author, born: args.setBornTo}
+
         authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
         return updatedAuthor
     }
--- osa8/library-frontend-main/src/queries.js
+++ osa8/library-frontend-main/src/queries.js
@@ -33,3 +33,14 @@ export const CREATE_BOOK = gql`
     }
   }
 `
+export const UPDATE_AUTHOR = gql`
+  mutation updateAuthor ($name: String!, $setBornTo: Int!){
+    editAuthor(
+        name: $name,
+        setBornTo: $setBornTo
+    ){
+        name,
+        setBornTo
+    }
+  }
+`
